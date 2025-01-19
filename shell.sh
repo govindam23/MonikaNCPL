b4f91c8a6697f70fbaee112eace1e724a1d7ca37
mgovind23 <govindam@uwindsor.ca>
2025-01-19 09:58:50 -0500

    add new files

--- shell.sh
+++ shell.sh
@@ -1,28 +1,40 @@
-#!/bin/bash 
+#!/bin/bash
 
-source /home/mgovind23/config.env
+# Load environment variables from config.env file
+if [ -f ./config.env ]; then
+    export $(cat ./config.env | xargs)
+fi
 
+# Define variables for resource group
+RESOURCE_GROUP_NAME="project-6"
+LOCATION="canadaeast"
 
-echo "Logging into Azure with created Service Principal" 
+# Log in to Azure using the service principal
+az login --service-principal --username $AZURE_APP_ID --password $AZURE_PASSWORD --tenant $AZURE_TENANT
 
-az login --service-principal -u $AZURE_APP_ID -P $AZURE_PASSWORD --tenant $AZURE_TENANT 
+# Check login status
+if [ $? -eq 0 ]; then
+    echo "Login successful!"
 
+    # Create the resource group
+    az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+    
+    # Check resource group creation status
+    if [ $? -eq 0 ]; then
+        echo "Resource group creation successful!"
 
-if [$? -ne 0 ]; then 
-   echo "Oops Azure login failed.." 
-    exit 1 
-fi 
-echo "Yayy, Azure login successful" 
-
- 
-
-echo "Deploying ARM template to create storage account"
-
- 
-if [$? -ne 0 ]; then 
-  echo "Deployment failed" 
-  exit 1 
-fi 
-
-echo "Storage account deployed successfully!" 
-az deployment group create --resource-group NCPLproject --template-file storage_account.json 
+        # Deploy the ARM template to create the storage account within the resource group
+        az deployment group create --resource-group $RESOURCE_GROUP_NAME --template-file storage_account.json --parameters @parameters.json
+        
+        # Check deployment status
+        if [ $? -eq 0 ]; then
+            echo "Storage account creation successful!"
+        else
+            echo "Storage account creation failed!"
+        fi
+    else
+        echo "Resource group creation failed!"
+    fi
+else
+    echo "Login failed!"
+fi
